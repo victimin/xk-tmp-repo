f1320e6cf8c0f2dfc9b5c6ad435c3bb8bf48cd0f
Jerry Lee <jerry@xandarkardian.com>
2020-05-25 15:35:24 +0900

    willowbrook sdk

---
+++ willowbrook_SDK/XK_SDK_V_2_04_05/dep/subHttpClient.d
@@ -0,0 +1,4 @@
+obj/subHttpClient.o: src/subHttpClient.c src/parson.h src/RadarCommand.h \
+ src/XK_CommonDefineSet.h src/XK_tools.h src/XK_RadarApplicationInfo.h \
+ src/subHttpClient.h src/Plugin_falldetection.h \
+ src/Plugin_inoutCounting.h
