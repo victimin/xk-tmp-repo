69041c29fb0635486918c213931280373b123395
Jerry Lee <jerry@xandarkardian.com>
2020-06-09 23:23:58 +0900

    Debug-ver

--- calibration.sh
+++ calibration.sh
@@ -3,5 +3,9 @@ sudo killall xksdk
 sleep 5
 cd /home/xk/XK_SDK_V_2_04_05_mythings_Rdr_Debug_ver/
 sudo make dbgbin
+/home/xk/XK_SDK_V_2_04_05_mythings_Rdr_Debug_ver/tools/s2r 0 5028
+/home/xk/XK_SDK_V_2_04_05_mythings_Rdr_Debug_ver/tools/s2r 1 5028
+/home/xk/XK_SDK_V_2_04_05_mythings_Rdr_Debug_ver/tools/s2r 2 5028
+/home/xk/XK_SDK_V_2_04_05_mythings_Rdr_Debug_ver/tools/s2r 3 5028
+sleep 6
 sudo xksdk &
-
