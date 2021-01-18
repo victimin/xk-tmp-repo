bdd777301a437a4c75587148b67b9e5f4a64cacb
Jerry Lee <jerry@xandarkardian.com>
2021-01-18 15:07:54 +0900

    xksdk

--- checkap_reconnect.sh
+++ checkap_reconnect.sh
@@ -6,7 +6,7 @@ check_cmd=`cat /sys/class/net/ap0/operstate`
 
 if [[ "$check_cmd" == *"down"* ]];then
     sudo /root/shell/start-ap-managed-wifi.sh &
-    # echo "down"
-# else
-    # echo "up"
+    echo "down"
+else
+    echo "up"
 fi
